26853be3941285c0734126c62afd197fc6caf6ab
haarikkaota <haarikakota@gmail.com>
2020-11-23 10:55:16 -0600

    👌 IMPROVE: Lazy loading with modules & Building tables

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,3 +1,4 @@
+import { stripSummaryForJitNameSuffix } from '@angular/compiler/src/aot/util';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'Designer' },
+    { name: 'Jill', age: 26, job: 'Developer' },
+    { name: 'Elyse' ,age: 25, job: 'Engineer' } 
+  ];
+  headers= [
+    { key: 'name', label: 'Name' },
+    {key: 'age', label: 'Age' },
+    {key: 'job', label:'Job' }
+  ];
 
   constructor() { }
 
