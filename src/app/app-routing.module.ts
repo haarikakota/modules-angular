9888804e41952975ba54f67b4eff4c4507f75a05
haarikkaota <haarikakota@gmail.com>
2020-11-30 15:32:57 -0600

    👌 IMPROVE: Component routing and component reusability

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./views/views.module').then((h) => h.ViewsModule)
   },
+  {
+    path: 'mods',
+    loadChildren: () =>
+      import('./mods/mods.module').then((g) => g.ModsModule)
+  },
   { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent }
 ];
